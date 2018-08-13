71ce58e79be70d8cce2a376d027f6fe32791874f
Oksana Tkachenko <oksana@crystax.net>
2018-08-14 01:42:13 +0300

    added slider

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
 import $ from 'jquery';
-
 //import 'bootstrap/dist/css/bootstrap.min.css';
-console.log('webpack 4');
+
+import Swiper from 'swiper';
+
+var mySwiper = new Swiper('.swiper-container', {
+  slidesPerView: 5,
+  spaceBetween: 15,
+  /*
+      slidesPerGroup: 3,
+      loop: true,
+      loopFillGroupWithBlank: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      */
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+});
+
