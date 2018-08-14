1f5f67d9d2b93511053dd736dfa8387dd730acaa
Oksana Tkachenko <oksana@crystax.net>
2018-08-14 20:08:44 +0300

    changed grid for slider

--- src/index.js
+++ src/index.js
@@ -13,27 +13,30 @@ $(document).ready(function() {
  //   grabCursor: true,
 
     breakpoints: {
-    // when window width is <= 320px
-    320: {
+    // when window width is <= 375px
+    375: {
       slidesPerView: 1,
-      spaceBetween: 10
+      spaceBetween: 0
     },
-    // when window width is <= 480px
-    480: {
+    // when window width is <= 576px
+    576: {
       slidesPerView: 2,
       spaceBetween: 10
     },
-    // when window width is <= 640px
-    768: {
-      slidesPerView: 3,
-      spaceBetween: 20
+    // when window width is <= 740px
+    740: {
+      slidesPerView: 2,
+      spaceBetween: 15
     },
-
+    // when window width is <= 992px
     992: {
+      slidesPerView: 3,
+      spaceBetween: 10
+    },
+    1140: {
       slidesPerView: 4,
       spaceBetween: 15
     },
-
     1200: {
       slidesPerView: 5,
       spaceBetween: 15
