62141f2f8739e7c73d6210513762e2cb33fb99c6
Oksana Tkachenko <oksana@crystax.net>
2018-08-14 03:30:44 +0300

    added block search, WIP

--- src/index.js
+++ src/index.js
@@ -1,23 +1,19 @@
 import $ from 'jquery';
 //import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
 
 import Swiper from 'swiper';
 
-var mySwiper = new Swiper('.swiper-container', {
-  slidesPerView: 5,
-  spaceBetween: 15,
-  /*
-      slidesPerGroup: 3,
-      loop: true,
-      loopFillGroupWithBlank: true,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      */
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
+$(document).ready(function() {
+  var mySwiper = new Swiper('.swiper-container', {
+    slidesPerView: 5,
+    spaceBetween: 15,
+    slidesPerGroup: 3,
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
 });
 
