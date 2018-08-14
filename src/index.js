3013c36f0edbe76e4764118dcd0656a22df40d2a
Oksana Tkachenko <oksana@crystax.net>
2018-08-14 09:18:21 +0300

    change slider for mobile

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import $ from 'jquery';
 //import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap';
+//import 'bootstrap';
 
 import Swiper from 'swiper';
 
@@ -8,8 +8,36 @@ $(document).ready(function() {
   var mySwiper = new Swiper('.swiper-container', {
     slidesPerView: 5,
     spaceBetween: 15,
-    slidesPerGroup: 3,
-    loop: true,
+ //   grabCursor: true,
+
+    breakpoints: {
+    // when window width is <= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10
+    },
+    // when window width is <= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 10
+    },
+    // when window width is <= 640px
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 15
+    },
+
+    992: {
+      slidesPerView: 4,
+      spaceBetween: 15
+    },
+
+    1200: {
+      slidesPerView: 5,
+      spaceBetween: 15
+    }
+    },
+
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
