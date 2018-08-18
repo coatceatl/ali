90ff7b065af8376a2f2f8c40f110a20e9a0efcea
Oksana Tkachenko <oksana@crystax.net>
2018-08-19 01:50:49 +0300

    added handler for amount products

--- src/index.js
+++ src/index.js
@@ -87,6 +87,22 @@ $(document).ready(function() {
     $('#signup').removeClass('d-flex flex-column');
   });
 
+  /* amount products */
+  $('#minus').click(function () {
+    var $input = $(this).parent().find('input');
+    var count = parseInt($input.val()) - 1;
+    count = count < 1 ? 1 : count;
+    $input.val(count);
+    $input.change();
+    return false;
+  });
+  $('#plus').click(function () {
+    var $input = $(this).parent().find('input');
+    $input.val(parseInt($input.val()) + 1);
+    $input.change();
+    return false;
+  });
+
   /* yandex map */
 
   ymaps.ready(init);
